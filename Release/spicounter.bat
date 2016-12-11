f95a0413c3789a6e79f3633eb421db5138fe1ba0
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-11 17:33:34 -0500

    first commit

---
+++ Release/spicounter.bat
@@ -0,0 +1,7 @@
+start spicounter "CLOCK" -1 -1 1920 500 480
+rem start spicounter "COUNTUP" 0 -1 500 500 480
+start spicounter "COUNTUP" 120 300 100 100 880
+rem start spicounter "COUNTUP" 3600 -1 500 500 480
+rem start spicounter "COUNTDOWN" 3600 -1 500 500 480
+rem start spicounter "COUNTDOWN" 3600 3590 500 500 480
+rem start spicounter "COUNTUP" 1700 1710 500 400 800
